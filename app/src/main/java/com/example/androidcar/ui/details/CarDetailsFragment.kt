0b68f80cba06b76cad84f5bb861457b920433ee9
B.Thomas <begue.thomas@laposte.net>
2021-03-14 13:09:13 +0100

    Implements details views UI/UX

---
+++ app/src/main/java/com/example/androidcar/ui/details/CarDetailsFragment.kt
@@ -0,0 +1,32 @@
+package com.example.androidcar.ui.details
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.androidcar.R
+
+class CarDetailsFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = CarDetailsFragment()
+    }
+
+    private lateinit var viewModel: CarDetailsViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.car_details_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(CarDetailsViewModel::class.java)
+        // Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/androidcar/ui/details/CarDetailsViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.androidcar.ui.details
+
+import androidx.lifecycle.ViewModel
+
+class CarDetailsViewModel : ViewModel() {
+    // Implement the ViewModel
+}
